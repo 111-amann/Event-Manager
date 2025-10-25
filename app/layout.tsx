e69f51b224de0313b21387f2c204a6ffe217cf10
111-amann <amansayyad7887@gmail.com>
2025-10-26 00:15:22 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import {
+  ClerkProvider
+} from "@clerk/nextjs";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-poppins",
+});
+
+export const metadata: Metadata = {
+  title: "Sanvent",
+  description:
+    "Sanvent is a platform for event managment for Sanjivani University.",
+  icons: {
+    icon: "/assest/images/logo.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={poppins.variable}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
---
+++ components/ui/shared/NavItems.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navitems = () => {
+  return (
+    <ul className='md:flex-between flex'></ul>
+  )
+}
+
+export default Navitems
