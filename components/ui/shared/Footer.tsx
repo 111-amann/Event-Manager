60aa9a7848109adb3b3a7858a7f2961574c7a0ce
111-amann <amansayyad7887@gmail.com>
2025-10-26 11:14:58 +0530

    Update layout and add constants file

--- components/ui/shared/Footer.tsx
+++ components/ui/shared/Footer.tsx
@@ -1,7 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="border-t">
+      <div className="flex flex-center flex-between wrapper flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+        <Image src="/assets/images/logo.svg"  alt="logo" width={128} height={38}/>
+        </Link>
+
+       <p>2025 Sanvent. All rights reserved.</p>
+      </div>
+    </footer>
+  )
 };
 
 export default Footer;
