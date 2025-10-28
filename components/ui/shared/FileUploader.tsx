f5c817bf72a3c211b8a7c9dc5dca72bfd70c7394
kalpesh kawade <kalpeshkawade2@gmail.com>
2025-10-28 13:46:55 +0530

    kalpesh-create event updated

--- components/ui/shared/FileUploader.tsx
+++ components/ui/shared/FileUploader.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useCallback, Dispatch, SetStateAction } from "react";
-import type { FileWithPath } from "@uploadthing/react";
+import type { FileWithPath } from "react-dropzone";
 import { useDropzone } from "react-dropzone";
 import { generateClientDropzoneAccept } from "uploadthing/client";
 
